91b34910a4e4b70354ef269ce423e6cb3f5996dd
Julien Maille <julien.maille@gmail.com>
2025-01-19 13:34:31 +0100

    IMP: extract fonts and movies

--- README.md
+++ README.md
@@ -1,23 +1,16 @@
-# Bplist Image Extractor
+# Bplist Data Extractor
 
-This command line program extracts images from a `.bplist` file and saves them as PNG or JPEG files.
-
-## Requirements
-- Python 3.x
-- Pillow library
-
-## Installation
-1. Install the required libraries:
-   ```bash
-   pip install -r requirements.txt
-   ```
+This command line program extracts fonts, images and movies from a `bplist` file.
 
 ## Usage
 You can run the script using the following command:
 ```bash
 python main.py <bplist_file> [output_directory]
 ```
-Where `<bplist_file>` is the path to your `.bplist` file and `[output_directory]` is optional. If not specified, the script will use a default directory.
+Where `<bplist_file>` is the path to your `*.clock2` file and `[output_directory]` is optional. If not specified, the script will use a default directory based on the bplist filename.
 
-## Windows
+## Windows users
 You can simply drag and drop the file on `output\main.exe`
+
+## Note
+Please ask for permission and credit original author.
--- main.py
+++ main.py
@@ -2,54 +2,69 @@ import plistlib
 import os
 import sys
 import json
-from PIL import Image
 import base64
-import io
 
-def extract_image(image_data, output_dir, filename):
+def dump_file(raw_data, output_dir, filename):
     try:
-        image = Image.open(io.BytesIO(image_data))
-        filename, _ = os.path.splitext(filename)
-        output_file = os.path.join(output_dir, f'{filename}.{image.format.lower()}')
-        image.save(output_file)
-        print(f'Saved image: {output_file}')
-    except Exception as e:
-        print(f'Error extracting image for key {filename}: {e}')
+        # Check the first few bytes to determine the file type
+        if raw_data.startswith(b'\x00\x01\x00\x00'):
+            if not filename.endswith('.ttf'):
+                filename += '.ttf'
+        elif raw_data.startswith(b'\x89PNG'):
+            if not filename.endswith('.png'):
+                filename += '.png'
+        elif raw_data.startswith(b'\xff\xd8'):
+            if not filename.endswith('.jpg'):
+                filename += '.jpg'
+        elif raw_data.startswith(b'\x00\x00\x00\x18'):
+            if not filename.endswith('.mp4'):
+                filename += '.mp4'
+        elif raw_data.startswith(b'\x66\x74\x79\x70'):
+            if not filename.endswith('.mov'):
+                filename += '.mov'                
+        else:
+            print(f'Unknown file type for {filename}')
 
-def extract_images_recursive(data, output_dir, key_prefix=''):
+        with open(os.path.join(output_dir, filename), 'wb') as f:
+            f.write(raw_data)
+        print(f'Saved file: {os.path.join(output_dir, filename)}')
+    except Exception as e:
+        print(f'Error extracting file {filename}: {e}')
+
+def extract_files_recursive(data, output_dir, key_prefix=''):
     if isinstance(data, dict):
-        if 'profileName' in data:
+        if data.get('profileName'):
             # save the author name in Credits.txt
             with open(os.path.join(output_dir, 'Credits.txt'), 'w') as f:
-                f.write('Please ask for permission and credit original author:\n')
-                f.write(data['profileName'])
+                f.write(f'Please ask for permission and credit original author:\n{data.get('profileName')}')
                 if data.get('twitterHandle'):
-                    f.write(f'\nTwitter: {data.get('twitterHandle')}')
+                    f.write(f'\nTwitter: @{data.get('twitterHandle')}')
                 if data.get('tiktokHandle'):
-                    f.write(f'\nTiktok: {data.get('tiktokHandle')}')
+                    f.write(f'\nTiktok: @{data.get('tiktokHandle')}')
 
-        if data.get('imageData', None) and data.get('filename', ''):
-            extract_image(base64.b64decode(data['imageData']), output_dir, data['filename'])
+        if data.get('imageData', None):
+            filename = data.get('filename') if data.get('filename') else key_prefix
+            dump_file(base64.b64decode(data['imageData']), output_dir, filename)
         else:
             for key, value in data.items():
                 new_key_prefix = f'{key_prefix}{key}_'
-                extract_images_recursive(value, output_dir, new_key_prefix)
+                extract_files_recursive(value, output_dir, new_key_prefix)
     elif isinstance(data, bytes):
         try:
             # Check if the data starts with a valid JSON byte sequence or image data
             if data.startswith(b'{') or data.startswith(b'['):
                 json_data = json.loads(data.decode('utf-8'))
                 for key, value in json_data.items():
-                    extract_images_recursive(value, output_dir, key_prefix + key + '_')
+                    extract_files_recursive(value, output_dir, key_prefix + key + '_')
             else:
-                # Handle as image data directly if it's not JSON
-                extract_image(data, output_dir, key_prefix)
+                # Handle as raw data directly if it's not JSON
+                dump_file(data, output_dir, key_prefix)
         except Exception as e:
             print(f'Error extracting image for key {key_prefix}: {e}')
     elif isinstance(data, list):
         for index, item in enumerate(data):
             new_key_prefix = f'{key_prefix}{index}_'
-            extract_images_recursive(item, output_dir, new_key_prefix)
+            extract_files_recursive(item, output_dir, new_key_prefix)
 
 def extract_images_from_bplist(bplist_file, output_dir):
     # Load the binary plist file
@@ -59,24 +74,22 @@ def extract_images_from_bplist(bplist_file, output_dir):
     # Check if the plist data is a dictionary
     if not isinstance(plist_data, dict):
         print('Invalid plist format.')
-        return
-
-    # Extract images
-    extract_images_recursive(plist_data, output_dir)
+    else:
+        extract_files_recursive(plist_data, output_dir)
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print('Usage: python extract_images.py <bplist_file> [<output_directory>]')
+        print('Usage: python main.py <bplist_file> [<output_directory>]')
         sys.exit(1)
 
     bplist_file = sys.argv[1]
     if len(sys.argv) == 3:
         output_directory = sys.argv[2]
     elif len(sys.argv) == 2:
-        # Create output directory using the base name of the bplist file
+        # Name output directory using the base name of the bplist file
         base_name = os.path.splitext(os.path.basename(bplist_file))[0]
         output_directory = os.path.join(os.path.dirname(bplist_file), base_name)
-        os.makedirs(output_directory, exist_ok=True)
-        print(f'Output directory created: {output_directory}')
+    os.makedirs(output_directory, exist_ok=True)
+    print(f'Output directory created: {output_directory}')
 
     extract_images_from_bplist(bplist_file, output_directory)
